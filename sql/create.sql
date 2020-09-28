72facd43f3264647dcbfdd00a2ad20052169a0ca
boyko1989 <hunwiss89@gmail.com>
2020-09-28 09:40:02 +0400

    Добавил папку sql

---
+++ sql/create.sql
@@ -0,0 +1,10 @@
+/* Создание таблицы `users` */
+
+CREATE TABLE `users` (
+    `user_id` int(11) unsigned NOT NULL auto_increment,
+    `user_login` varchar(30) NOT NULL,
+    `user_password` varchar(32) NOT NULL,
+    `user_hash` varchar(32) NOT NULL default '',
+    `user_ip` int(10) unsigned NOT NULL default '0',
+    PRIMARY KEY (`user_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
