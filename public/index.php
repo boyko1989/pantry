35f0ff6ddf7ebb010cd8e2cc841184f12e40cfdc
boyko1989 <hunwiss89@gmail.com>
2020-09-30 19:16:18 +0400

    Наладил регистрацию!

--- public/index.php
+++ public/index.php
@@ -1,41 +1,40 @@
 <?php 
 session_start();
 require_once '../config/functions.php';
-//if (!isset($_COOKIE['id'])) {
-
-    echo '<pre>';
-    print_r($_SESSION);
-    echo '</pre>';
-if (!isset($_SESSION['hash'])) {
-    hello_form(); exit;
-} 
-
-if ($_SESSION['profile_guest'] = 'guest' AND $_SESSION['profile'] !== 'user') {
-    echo 'Гостевой профиль гостя ' . $_SESSION['login'];
-    bem_head('Главная');
-    bem_header('Мои файлы'); exit;
-} 
-
-if ($_SESSION['profile'] = 'user' AND $_SESSION['profile_guest'] = "")  {
-    bem_head('Главная');
-    bem_header('Мои файлы');
-    echo 'Приветствуем пользователя' . $_SESSION['login'] . '!';
-    ?>
-        <main>
-            <div class="doubleline"> 
-                <hr color="black" noshade size="2px"></div>
-            <div class="con_menu">
-    <?php  
-        menu1(); 
-        menu2();               
-        echo '</div><div class="doubleline3"></div>';
-        listing();
-    ?>    
-            <div class="doubleline"> 
-                <hr color="black" noshade size="2px">           
-            </div>
-        </main>      
-    </body>
-    </html>
-    <?php footer(); 
-} ?>
+//если имеется дескриптор пользователя, значит произведён вход, значит отображаем стартовую страницу сервиса
+    if ($_SESSION['hash'] != "") {
+        //сразу проверяем роль пользователя
+        if ($_SESSION['user'] === 'user') {
+            $role = 'полноправный зарегистрированный пользователь';
+        }   
+        if ($_SESSION['user'] === 'guest') {
+            $role = 'гость';
+        }
+        
+        bem_head('Главная');
+        bem_header('Мои файлы');
+        echo '<h1>Пользователь '.$_SESSION['login'].' вошёл как ' .$role.'!</h1>';
+        ?>
+            <main>
+                <div class="doubleline"> 
+                    <hr color="black" noshade size="2px"></div>
+                <div class="con_menu">
+        <?php  
+            menu1(); 
+            menu2();               
+            echo '</div><div class="doubleline3"></div>';
+            listing();
+        ?>    
+                <div class="doubleline"> 
+                    <hr color="black" noshade size="2px">           
+                </div>
+            </main>      
+        </body>
+        </html>
+        <?php footer();
+    } 
+    //если дескриптора пользователя нет, то отображаем приветственную страницу
+    else {
+        hello_form(); exit;
+    }
+?>
--- public/login.php
+++ public/login.php
@@ -1,33 +1,25 @@
 <?php session_start();
 require_once '../config/connect.php';
 require_once '../config/functions.php';
-
+//Допустим мы зарегистрированы и хотим войти. Для этого нам нужно получить ПОСТОМ данные для входа. О том, что они имеются нам скажет нажатая кнопка submit.
+if (isset($_POST['submit'])) {
+//подключаемся к БД с целью найти пару логин и пароль
 //если в массиве ПОСТ появляется значение в ключе логин, тогда делаем запрос к БД
-if(isset($_POST['login'])) {
-    $query = mysqli_query($link,"SELECT user_id, user_password FROM users WHERE user_login='".mysqli_real_escape_string($link,$_POST['login'])."' LIMIT 1");
-    $data = mysqli_fetch_assoc($query);
-
-    // Сравниваем пароли
-    if($data['user_password'] === md5(md5($_POST['password']))) {
-        // Генерируем случайное число, шифруем его и добавляем в БД
-        //$hash = md5(generateCode(10));
-        $hash = md5(generateCode(10));
-
-        mysqli_query($link, "UPDATE users SET user_hash='".$hash."' WHERE user_id='".$data['user_id']."'");
+$res = mysqli_query($link,"SELECT user_id, user_password FROM users WHERE user_login='".mysqli_real_escape_string($link,$_POST['login'])."' AND user_password='".mysqli_real_escape_string($link,$_POST['password'])."' LIMIT 1");
+//Если запрос вернул 0 строк, значит пользователя нет и входа быть не может - отправляемся на страницу регистрации соответствующим сообщением
+if (mysqli_num_rows($res) == 0) {
+    $_SESSION['message'] = 'Такой пользователь не зарегистрирован';
+    header("Location: register.php"); exit;
+} //Если результат есть, то формируем массив $_SESSION и отправляемся в индексный файл
+else {
         $_SESSION['login'] = $_POST['login'];
-        $_SESSION['hash'] = $hash;
-        $_SESSION['profile'] = 'user';
-        /*
-        echo '<pre>';
-        print_r($_SESSION);
-        echo '</pre>';*/
-    
-        // Переадресовываем браузер на старт
-        header("Location: /"); exit();
-    }
-    else
-    {
-        print "Вы ввели неправильный логин/пароль";
+        $_SESSION['hash'] = md5(time()); //дескриптор пользователя
+        $res = mysqli_query($link,"UPDATE `users` SET `user_hash` = '".$_SESSION['hash']."' WHERE `users`.`user_login` = '".$_POST['login']."'");
+        $res_role = mysqli_query($link, "SELECT `user_role` FROM `users` WHERE `user_login` = '".$_POST['login']."'");
+        $res_role = mysqli_fetch_assoc($res_role);
+        $role = $res_role['user_role'];
+        $_SESSION['user'] = $role;
+        header("Location: /");
     }
 }
 ?>
@@ -50,11 +42,12 @@ if(isset($_POST['login'])) {
                     <input type="text" name="login" required><br><br>
 
                     <label>Пароль</label><br><br>
-                    <input type="password" name="password" required><br><br>
+                    <input type="password" name="password"><br><br>
 
                     <input name="submit" type="submit" value="Войти"><br><br>
                 </form> 
+                <?php if (isset($_SESSION)) {echo $_SESSION['message'];}?>
             </main>
         </div>      
     </body>
-</html>           
+</html>       
